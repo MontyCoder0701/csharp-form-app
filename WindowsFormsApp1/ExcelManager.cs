c26e10de99eef9a0a21b4197fd2527ae1b6c3902
Soojeong <soojlee0106@naver.com>
2024-12-10 17:15:39 +0900

    feat: 첫 커밋

---
+++ WindowsFormsApp1/ExcelManager.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using ClosedXML.Excel;
+using Newtonsoft.Json.Linq;
+
+namespace WindowsFormsApp1
+{
+    internal class ExcelManager
+    {
+        public static void ExportJsonToExcel(string fileName, string filePath, string jsonData)
+        {
+
+            var dataArray = JArray.Parse(jsonData);
+
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Data");
+
+                var headers = dataArray[0] as JObject;
+                int col = 1;
+                foreach (var header in headers.Properties())
+                {
+                    worksheet.Cell(1, col++).Value = header.Name;
+                }
+
+                int row = 2;
+                foreach (var item in dataArray)
+                {
+                    col = 1;
+                    foreach (var value in item.Values())
+                    {
+                        worksheet.Cell(row, col++).Value = value.ToString();
+                    }
+                    row++;
+                }
+
+                string savePath = Path.Combine(filePath, $"{fileName}.xlsx");
+                workbook.SaveAs(savePath);
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void HandleExportButtonClick(object sender, EventArgs e)
+        {
+            string data = @"[
+                { 'Name': 'Alice', 'Age': 25, 'City': 'New York' },
+                { 'Name': 'Bob', 'Age': 30, 'City': 'Los Angeles' },
+                { 'Name': 'Charlie', 'Age': 22, 'City': 'Chicago' }
+            ]";
+
+            string fileName = "엑셀파일임";
+            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            ExcelManager.ExportJsonToExcel(fileName, filePath, data);
+            MessageBox.Show("파일이 저장되었습니다.");
+        }
+    }
+}

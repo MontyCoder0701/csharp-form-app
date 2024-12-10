7619aa2b978a80eb5177a818cce705f89340e23a
Soojeong <soojlee0106@naver.com>
2024-12-10 17:47:45 +0900

    feat: 그리드뷰와 엔티티 추가

---
+++ WindowsFormsApp1/BankTransaction.cs
@@ -0,0 +1,53 @@
+﻿namespace WindowsFormsApp1
+{
+    internal class BankTransaction
+    {
+        public int TranSeq;
+        public int AcctSeq;
+        public int? Balance100;
+        public int? Bcode;
+        public string Cid;
+        public string Date;
+        public string Description;
+        public int? Order;
+        public int? Pay100;
+        public int? Save100;
+        public string Time;
+        public string Trankind;
+        public int TransferSeq;
+        public string Where;
+
+        public BankTransaction(
+            int tranSeq,
+            int acctSeq,
+            int? balance100,
+            int? bcode,
+            string cid,
+            string date,
+            string description,
+            int? order,
+            int? pay100,
+            int? save100,
+            string time,
+            string trankind,
+            int transferSeq,
+            string where
+        )
+        {
+            TranSeq = tranSeq;
+            AcctSeq = acctSeq;
+            Balance100 = balance100;
+            Bcode = bcode;
+            Cid = cid;
+            Date = date;
+            Description = description;
+            Order = order;
+            Pay100 = pay100;
+            Save100 = save100;
+            Time = time;
+            Trankind = trankind;
+            TransferSeq = transferSeq;
+            Where = where;
+        }
+    }
+}
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,6 +12,43 @@ namespace WindowsFormsApp1
 
         private void HandleExportButtonClick(object sender, EventArgs e)
         {
+            // TODO: 위치 분리하기
+            BankTransaction[] transactions = new BankTransaction[]
+            {
+                new BankTransaction(
+                    tranSeq: 1,
+                    acctSeq: 1001,
+                    balance100: 50000,
+                    bcode: 1234,
+                    cid: "CUST001",
+                    date: "2024-12-10",
+                    description: "Transaction 1 Description",
+                    order: 1,
+                    pay100: 2000,
+                    save100: 3000,
+                    time: "10:30 AM",
+                    trankind: "Debit",
+                    transferSeq: 2001,
+                    where: "ATM1"
+                ),
+                new BankTransaction(
+                    tranSeq: 2,
+                    acctSeq: 1002,
+                    balance100: 100000,
+                    bcode: 5678,
+                    cid: "CUST002",
+                    date: "2024-12-09",
+                    description: "Transaction 2 Description",
+                    order: 2,
+                    pay100: 5000,
+                    save100: 7000,
+                    time: "11:15 AM",
+                    trankind: "Credit",
+                    transferSeq: 2002,
+                    where: "Branch1"
+                ),
+             };
+
             string data = @"[
                 { 'Name': 'Alice', 'Age': 25, 'City': 'New York' },
                 { 'Name': 'Bob', 'Age': 30, 'City': 'Los Angeles' },

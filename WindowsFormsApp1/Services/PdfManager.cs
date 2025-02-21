867d8d326d0389ec60793bd2c6ba22c932bc8e32
Soojeong <soojlee0106@naver.com>
2025-02-21 10:46:47 +0900

    refactor: 함수 정리

--- WindowsFormsApp1/Services/PdfManager.cs
+++ WindowsFormsApp1/Services/PdfManager.cs
@@ -9,69 +9,62 @@ namespace WindowsFormsApp1.Services
     {
         public static List<List<string>> ImportPdfToTable(string pdfPath, int pageNum = 1, int rowThreshold = 5, int cellThreshold = 30)
         {
-            List<List<(string text, double x, double y)>> tableRows = new List<List<(string, double, double)>>();
-
-            using (PdfDocument document = PdfDocument.Open(pdfPath))
+            using (var document = PdfDocument.Open(pdfPath))
             {
-
-                var page = document.GetPage(pageNum);
-                var words = page.GetWords()
-                    .Select(word => (word.Text, word.BoundingBox.Left, word.BoundingBox.Bottom))
+                var words = document.GetPage(pageNum).GetWords()
+                    .Select(w => new { w.Text, X = w.BoundingBox.Left, Y = w.BoundingBox.Bottom })
+                    .OrderByDescending(w => w.Y)
                     .ToList();
 
-                words.Sort((a, b) => b.Bottom.CompareTo(a.Bottom));
-
-                List<(string, double, double)> currentRow = new List<(string, double, double)>();
+                var tableRows = new List<List<(string text, double x)>>();
+                var currentRow = new List<(string, double)>();
                 double lastY = double.MaxValue;
 
-                foreach (var (text, x, y) in words)
+                foreach (var word in words)
                 {
-                    if (Math.Abs(y - lastY) > rowThreshold)
+                    if (Math.Abs(word.Y - lastY) > rowThreshold && currentRow.Count > 0)
                     {
-                        if (currentRow.Count > 0)
-                        {
-                            tableRows.Add(new List<(string, double, double)>(currentRow));
-                        }
+                        tableRows.Add(new List<(string, double)>(currentRow));
                         currentRow.Clear();
                     }
-                    currentRow.Add((text, x, y));
-                    lastY = y;
+                    currentRow.Add((word.Text, word.X));
+                    lastY = word.Y;
                 }
 
                 if (currentRow.Count > 0)
                 {
                     tableRows.Add(currentRow);
                 }
-            }
 
-            List<List<string>> structuredTable = new List<List<string>>();
+                var structuredTable = new List<List<string>>();
 
-            foreach (var row in tableRows)
-            {
-                row.Sort((a, b) => a.x.CompareTo(b.x));
-
-                List<string> mergedRow = new List<string>();
-                string currentCell = row[0].text;
-                double lastX = row[0].x;
-
-                for (int i = 1; i < row.Count; i++)
+                foreach (var row in tableRows)
                 {
-                    if (Math.Abs(row[i].x - lastX) < cellThreshold)
-                    {
-                        currentCell += " " + row[i].text;
-                    }
-                    else
-                    {
-                        mergedRow.Add(currentCell);
-                        currentCell = row[i].text;
-                    }
-                    lastX = row[i].x;
-                }
-                mergedRow.Add(currentCell);
-                structuredTable.Add(mergedRow);
-            }
+                    row.Sort((a, b) => a.x.CompareTo(b.x));
 
-            return structuredTable;
+                    var mergedRow = new List<string>();
+                    string currentCell = row[0].text;
+                    double lastX = row[0].x;
+
+                    for (int i = 1; i < row.Count; i++)
+                    {
+                        if (Math.Abs(row[i].x - lastX) < cellThreshold)
+                        {
+                            currentCell += " " + row[i].text;
+                        }
+                        else
+                        {
+                            mergedRow.Add(currentCell);
+                            currentCell = row[i].text;
+                        }
+                        lastX = row[i].x;
+                    }
+                    mergedRow.Add(currentCell);
+                    structuredTable.Add(mergedRow);
+                }
+
+                return structuredTable;
+            }
         }
     }
 }
